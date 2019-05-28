76201eea41df6c205589c891db4b9dfa1810fa37
Michael Smith <msmthey@amazon.com>
2019-05-28 15:36:51 -0700

    added initial project component and service

--- songs-client/src/app/app.module.ts
+++ songs-client/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { LoginRegComponent } from './login-reg/login-reg.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    LoginRegComponent
   ],
   imports: [
     BrowserModule
