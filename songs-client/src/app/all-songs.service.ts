e89f8e0b7f55ffb9063330ca363ab0f0c07a36ef
Cameron Wilkes <cwilkes17@gmail.com>
2019-05-29 14:48:25 -0700

    added some things

--- songs-client/src/app/all-songs.service.ts
+++ songs-client/src/app/all-songs.service.ts
@@ -1,9 +1,20 @@
+
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AllSongsService {
+  baseUrl: string = 'http://localhost:8000/songs'
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  createSong(addSongData: object): Observable<object> {
+    return this.http.post<object>(`${this.baseUrl}/create/`, addSongData);
+  }
+
+  getSongs(): Observable<any[]>{
+    return this.http.get<any[]>(`${this.baseUrl}/list/`);
+  }
 }
--- songs-client/src/app/app.module.ts
+++ songs-client/src/app/app.module.ts
@@ -4,15 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import {FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
 import { LoginRegComponent } from './login-reg/login-reg.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { AddSongComponent } from './add-song/add-song.component';
 import { AllSongsComponent } from './all-songs/all-songs.component';
 import { SingleSongComponent } from './single-song/single-song.component';
-import { PlaylistComponent } from './playlist/playlist.component';
+
 
 @NgModule({
   declarations: [
@@ -20,10 +18,9 @@ import { PlaylistComponent } from './playlist/playlist.component';
     UsersComponent,
     LoginRegComponent,
     NavbarComponent,
-    AddSongComponent,
     AllSongsComponent,
     SingleSongComponent,
-    PlaylistComponent
+
   ],
   imports: [
     BrowserModule,
